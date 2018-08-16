6d80fa2fd0c377abf4c57f656539f9081eb7e811
Mikhail Temkine <miktemk@gmail.com>
2018-08-16 19:53:13 -0400

    MT - add seedColor parameter to ShowDialog

--- src/ColorPickerWPF/ColorPickerWindow.xaml.cs
+++ src/ColorPickerWPF/ColorPickerWindow.xaml.cs
@@ -17,8 +17,8 @@ namespace ColorPickerWPF
         {
             InitializeComponent();
         }
-
-        public static bool ShowDialog(out Color color, ColorPickerDialogOptions flags = ColorPickerDialogOptions.None, ColorPickerControl.ColorPickerChangeHandler customPreviewEventHandler = null)
+        
+        public static bool ShowDialog(out Color color, Color? seedColor = null, ColorPickerDialogOptions flags = ColorPickerDialogOptions.None, ColorPickerControl.ColorPickerChangeHandler customPreviewEventHandler = null)
         {
             if ((flags & ColorPickerDialogOptions.LoadCustomPalette) == ColorPickerDialogOptions.LoadCustomPalette)
             {
@@ -26,6 +26,7 @@ namespace ColorPickerWPF
             }
 
             var instance = new ColorPickerWindow();
+            instance.ColorPicker.SetColor(seedColor ?? Colors.White);
             color = instance.ColorPicker.Color;
 
             if ((flags & ColorPickerDialogOptions.SimpleView) == ColorPickerDialogOptions.SimpleView)
